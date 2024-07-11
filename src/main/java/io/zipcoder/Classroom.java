e7a9c0a420afdead26466bc117e520efada01503
Will <will@Zipcoders-MacBook-Pro-9.local>
2024-07-11 18:56:39 -0400

    made changes

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,7 +1,50 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Classroom {
-    Student [] students;
+  private   Student[] students;
+
+    public Classroom(int maxNumberOfStudents){
+    this.students = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom(Student[] students){
+        this.students =students;
+
+    }
+
+    public Classroom(){
+        this.students = new Student[30];
+
+
+    }
+
+    public Student[] getStudents(){
+
+
+
+        return students;
+    }
+
+    public double getAverageExamScore(){
+        double totalSum = 0;
+        int studentSize = students.length;
+
+        //Calculate sum of all exam scores
+        for (Student student : students){
+            String examScores = student.getExamScores();
+            double sum = 0;
+        }
+
+//        for(double score : examScores){
+//            sum += exam;
+//        }
+
+
+        return 4.0;
+    }
+
 
 
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,29 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ClassroomTest {
+
+    @Test
+    public void getStudentsTest() {
+        //Given
+        ArrayList<Double> examScoreBill = new ArrayList<>(Arrays.asList(88.1,33.5));
+        ArrayList<Double> examScoreWill = new ArrayList<>(Arrays.asList(88.1,33.5));
+        Student[] student = {new Student("Bill","Sill", examScoreBill),
+        new Student("Will","Pill",examScoreWill)};
+
+        Classroom classroom = new Classroom(student);
+
+        //When
+        Student[] actualStudents = classroom.getStudents();
+
+        //Then
+        Assert.assertArrayEquals(student,actualStudents);
+
+
+    }
 }
